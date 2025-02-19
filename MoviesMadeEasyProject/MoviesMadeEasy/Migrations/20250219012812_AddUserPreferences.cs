bdc57b60ff6a910068525e596798e7b0aafa897a
Kira Morgan <132946416+kiraryder@users.noreply.github.com>
2025-02-18 17:42:04 -0800

    Added Razor Runtime Compilation and Updated User Model (#72)
    * Entity Framework and Identity Connected to Project
    
    * Fixed Identity and added 'Areas > Identity > Pages'
    
    * Registration and Login are functional
    
    * Added Razor Runtime Compilation and Updated User Model

---
+++ MoviesMadeEasyProject/MoviesMadeEasy/Migrations/20250219012812_AddUserPreferences.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MoviesMadeEasy.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserPreferences : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ColorMode",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "FontSize",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "FontType",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ColorMode",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "FontSize",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "FontType",
+                table: "AspNetUsers");
+        }
+    }
+}
