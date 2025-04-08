93fe74faef32c0ec1cc59811472aed7469d68c7f
aroy23wou <aroy23@mail.wou.edu>
2025-04-07 17:09:47 -0700

    Bdd (#109)
    * Updated error messages
    
    * test .feature file added.:

---
+++ MoviesMadeEasyProject/MyBddProject.Tests/Login.feature
@@ -0,0 +1,5 @@
+Feature: Login
+  Scenario: Successful login
+    Given the user is on the login page
+    When the user enters valid credentials
+    Then the user should be redirected to the dashboard
---
+++ MoviesMadeEasyProject/MyBddProject.Tests/LoginSteps.cs
@@ -0,0 +1,23 @@
+using Reqnroll;
+
+[Binding]
+public class LoginSteps
+{
+    [Given("the user is on the login page")]
+    public void GivenTheUserIsOnTheLoginPage()
+    {
+        // Arrange 
+    }
+
+    [When("the user enters valid credentials")]
+    public void WhenTheUserEntersValidCredentials()
+    {
+        // Act
+    }
+
+    [Then("the user should be redirected to the dashboard")]
+    public void ThenTheUserShouldBeRedirectedToTheDashboard()
+    {
+        // Assert 
+    }
+}
